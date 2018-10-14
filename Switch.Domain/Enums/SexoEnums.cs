b5a1d2d3c2abe83ac3940b20b16badfef6c77480
ivonaldo da silva <ivonaldo1@msn.com>
2018-10-14 17:08:53 -0300

    Ferramentas Domain

---
+++ Switch.Domain/Enums/SexoEnums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Enums
+{
+    public enum SexoEnum
+    {
+        Naodefenido=1,
+        Femenino = 2,
+        Masculino =3
+
+    }
+}
